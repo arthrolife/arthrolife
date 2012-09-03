09e8f0eb15a17cf34e3db21f300faa171a6db17f
support <support@arthrolife.com.au>
2012-09-03 17:46:55 +1000

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
 window.onload = init;
 
+var ask = null;
+var askButton = null;
+
 function init() {
+  ask = document.querySelector('#ask');
+  askButton = document.querySelector('#ask-button');
+
+  askButton.onclick = function(event) {
+    ask.classList.toggle('ask-show');
+  };
 }
 
 
