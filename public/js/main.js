0200c7dd32e23bc9d224fad39d0c48181534e73c
support <support@arthrolife.com.au>
2012-09-05 13:01:22 +1000

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -21,9 +21,6 @@ function init() {
     var http = new XMLHttpRequest();
     http.open('POST', url, true);
     http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    http.onreadystatechange = function() {
-      if (http.readyState == 4 && http.status == 200) alert(http.responseText);
-    }
     http.send(params);
     toggleAskShow();
   }
